a6c466874bd39629b4e6f22840274fcaad6c7bc7
DanFakePlasticTrees <34700952+DanFakePlasticTrees@users.noreply.github.com>
2019-03-19 11:48:53 -0300

    Add files via upload

---
+++ Clase02/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*   int numeroUno;
+   int numeroDos;
+   int suma;
+   float promedio;
+
+   printf("Ingrese un número: ");
+   scanf("%d", &numeroUno);
+   printf ("ingrese otro número: ");
+   scanf("%d", &numeroDos);
+   suma= numeroUno + numeroDos;
+   promedio = (float)suma/2;
+   printf("El promedio es: %.1f", promedio);
+
+
+char palabra[15];
+printf("Ingrese una palabra: ");
+scanf("%s", palabra);
+printf("usted ingresó: %s", palabra);
+
+ int i =10;
+while (i>0) {
+
+    printf("\n Este es el valor de i: %d-", i);
+    i--;
+
+}
+
+int j;
+for(j=10;j >0;j--){
+ printf("\n Valor de j: %d", j);
+}
+
+int i;
+for(i=0;i<10;i++)
+{
+    if(i==5)
+        {
+    continue;
+    }
+
+printf("%d-", i);
+}*/
+
+
+    return 0;
+}
